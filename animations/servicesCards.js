0848646619d855ef787f3ae1af982063daa6b5c4
MAERA <maera.contaret@outlook.fr>
2023-08-11 09:44:36 +0200

    services card animation mobile version

--- animations/servicesCards.js
+++ animations/servicesCards.js
@@ -1,25 +1,44 @@
 
     const cards = document.querySelectorAll(".appear");
+    const faceImg = document.querySelectorAll(".face-image")
 
     function handleScroll() {
         const scrolledFromTop = window.pageYOffset + window.innerHeight;
 
         cards.forEach((card) => {
             const distanceFromTop = card.offsetTop;
-            const delaiAnim = card.getAttribute("data-delai");
+            const delaiAnim = card.getAttribute("data-delai")
 
             if (scrolledFromTop >= distanceFromTop + 50) {
                 setTimeout(() => {
-                    card.style.top = "0";
-                    card.style.opacity = "1";
-                }, delaiAnim);
+                    card.style.top = "0"
+                    card.style.opacity = "1"
+                
+                }, delaiAnim)
             }
-        });
+        })
+    }
+
+    function mobileVersion() {
+        const scrolledFromTop = window.pageYOffset + window.innerHeight;
+
+        faceImg.forEach((img) => {
+            const distanceFromTop = img.offsetTop
+            const delaiAnim = img.getAttribute("data-delai")
+
+            if (scrolledFromTop >= distanceFromTop + 50 && window.innerWidth <= 978) {
+                setTimeout(() => {
+                    img.style.height = "60px"
+                }, delaiAnim)
+            }
+        })
     }
 
     function init() {
-        handleScroll();
-        window.addEventListener('scroll', handleScroll);
+        handleScroll()
+        mobileVersion()
+        window.addEventListener('scroll', handleScroll)
+        window.addEventListener('scroll', mobileVersion)
     }
 
     init()
